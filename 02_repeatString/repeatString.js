98a9a0ef8c44dbf76ed1cf926f20e884bb93b6d2
Peter Katz <peter.nichols.katz@gmail.com>
2022-11-11 11:45:37 -0500

    complete tasks

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let result = '';
+        for (let i = 1; i <= num; i++){
+            result += string;  
+        }; 
+        if (num < 0) {
+            result = 'ERROR';
+        };
+    return result;
 };
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let splitString = string.split(''); //splits string into array
+    let reverseStrAray = splitString.reverse(); //reverses array
+    let revStr = ''; //= reverseStrAray[0].concat(reverseStrAray[1],reverseStrAray[2],reverseStrAray[3],reverseStrAray[4]); //concats array back together.. replace w loop?
 
+    for (i = 0; i < reverseStrAray.length; i++){
+        revStr += reverseStrAray[i];
+    }
+    return revStr; //returns reverse string
 };
 
 // Do not edit below this line
