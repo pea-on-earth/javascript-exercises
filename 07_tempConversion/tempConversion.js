3d49a2b3b8f214c1e244c2d196c4ebd81aab2778
Peter Katz <peter.nichols.katz@gmail.com>
2022-11-15 15:03:42 -0500

    update functions to pass all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  //F - 32 * 5/9 = C  (5/9)=.5556
+  let cels = Number(((temp - 32) * .5556).toFixed(1));
+  return cels;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  // C * 9/5 + 32 = F (9/5)=1.8
+  let far = Number(((temp * 1.8) + 32).toFixed(1));
+  return far;
 };
 
 // Do not edit below this line
